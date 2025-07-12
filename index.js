125a37cded6f78da9109388ad8c48e6a6af36cdb
yaazhini2006 <24bct162@kgcas.com>
2025-07-12 10:44:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var num=11;
+var name='js';
+var iscloudy=true;
+console.log(num+" "+name+ " " +iscloudy);
+document.write(num);
+document.write(name);
+document.write(iscloudy);
+//task2
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+//task3
+//ar num1=12;
+//var num2=23;
+var num1=parseInt(prompt("Enter the first vakue"));
+var num2=parseInt(prompt("enter the second value"));
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
